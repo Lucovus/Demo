f01522b5c9c2bd4a846b63848ff715147671e695
Lucovus <129883444+Lucovus@users.noreply.github.com>
2026-05-25 00:05:30 +0300

    ISP.sh

---
+++ ISP.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Выберете гипервизор. Proxmox или WMware (пиши без скобок 1 или 2)"
+read -p "2) - proxmox 1) - WMware" Hyperivoz
+case $Hyperizor in
+  1)
+    int_type="enp33"
+    ;;
+  2)
+    int_type="enp7s1"
+    ;;
+  *)
+    echo "Неправльно. напиши 1 или 2"
+    exit 1 
+    ;;
+esac
+
+echo "Используется "$int_type"
