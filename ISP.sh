53e5aa056667049846367b4a30d211a959a027cb
Lucovus <129883444+Lucovus@users.noreply.github.com>
2026-05-25 04:57:01 +0300

    ISP.sh

--- ISP.sh
+++ ISP.sh
@@ -37,7 +37,42 @@ else
   echo ""
 fi
 
+apt-get install nginx -y
+cat << "EOF" > /etc/nginx/sites-available.d/r-proxy.conf
+server {
+    listen 80;
+    server_name web.au-team.irpo;
 
+    location / {
+        proxy_pass http://172.16.1.10:8080;
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
 
+        auth_basic "Restricted Access";
+        auth_basic_user_file /etc/nginx/.htpasswd;
+    }
+}
+
+server {
+    listen 80;
+    server_name docker.au-team.irpo;
+
+    location / {
+        proxy_pass http://172.16.2.10:8080;
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+    }
+}
+EOF
+echo -e "\e[32mNginx файл конфига готов\e[0m"
+ln -s /etc/nginx/sites-available.d/r-proxy.conf /etc/nginx/sites-enabled.d/
+systemctl enable --now nginx
+apt-get install apache2-htpasswd -y
+read -p "Какой пароль указан в задани для Apache2? Напиши без пробелов" pass_apache
+htpasswd -c /etc/nginx/.htpasswd $pass_apache
 
 exec bash
