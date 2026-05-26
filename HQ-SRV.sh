e2adb51919e5835d3e6336d44dd91fa27fcd4cf1
Lucovus <129883444+Lucovus@users.noreply.github.com>
2026-05-27 00:23:04 +0300

    Update HQ-SRV.sh

--- HQ-SRV.sh
+++ HQ-SRV.sh
@@ -17,4 +17,53 @@ read -p "какой порт у ssh по заданию? Без пробелов
 read -p "Сколько попыток авторизации по заданию? Без пробелов только цифру " Max_auth
 echo -e "Port ${port_ssh}\nMaxAuthTries ${Max_auth}\nAllowUsers sshuser\nBanner /etc/openssh/banner\n" >> /etc/openssh/sshd_config
 systemctl restart sshd
+
+apt-get update && apt-get install bind bind-utils -y
+cat <<'EOF' > /etc/bind/options.conf
+logging { };
+options {
+ listen-on { any; };
+ forwarders { 77.88.8.7; 77.88.8.3; };
+ recursion yes;
+ allow-recursion { any; };
+ allow-query { any; };
+ dnssec-validation no;
+ 
+ directory "/etc/bind/zone";
+ dump-file "/var/run/named/named_dump.db";
+ statistics-file "/var/run/named/named.stats";
+ recursing-file "/var/run/named/named.recursing"; 
+ secroots-file "/var/run/named/named.scroots";
+ pid-file none;
+};
+zone "au-team.irpo" {
+ type master;
+ file "au-team.irpo";
+};
+zone "168.192.in-addr.arpa" {
+ type master;
+ file "168.192.in-addr.arpa";
+};
+EOF
+read -p "Напишите IP интерфейса enp2s1 без маски на HQ-RTR " ip_br-rtr
+cat <<'EOF' > /etc/bind/zone/au-team.irpo
+$TTL  1D
+@    IN   SOA   au-team.irpo. root.au-team.irpo. (
+                2025020600 ; serial
+                12H        ; refresh
+                1H         ; retry
+                1W         ; expire
+                1H         ; ncache
+            )
+@       IN  NS    hq-srv.au-team.irpo.
+hq-rtr  IN   A    192.168.100.1
+hq-srv  IN   A    192.168.100.2
+hq-cli  IN   A    192.168.200.2
+br-rtr  IN   A    ${ip_br-rtr}
+br-srv  IN   A    192.168.1.2
+docker  IN   A    172.16.1.1
+web     IN   A    172.16.2.1
+
+EOF
+
 exec bash
