da651f1d42c709e82383754933be8644a3253512
Lucovus <129883444+Lucovus@users.noreply.github.com>
2026-05-27 02:52:22 +0300

    Update HQ-SRV.sh

--- HQ-SRV.sh
+++ HQ-SRV.sh
@@ -47,7 +47,7 @@ zone "168.192.in-addr.arpa" {
 EOF
 read -p "Напишите IP интерфейса enp2s1 без маски который на BR-RTR " ip_br_rtr
 read -p "Напишите IP интерфейса enp2s1 без маски который на HQ-RTR " ip_hq_rtr
-cat <<'EOF' > /etc/bind/zone/au-team.irpo
+cat <<"EOF" > /etc/bind/zone/au-team.irpo
 $TTL  1D
 @    IN   SOA   au-team.irpo. root.au-team.irpo. (
                 2025020600 ; serial
@@ -63,7 +63,7 @@ hq-cli  IN   A    192.168.200.2
 br-rtr  IN   A    192.168.1.1
 br-srv  IN   A    192.168.1.2
 docker  IN   A    ${ip_hq_rtr}
-web     IN   A    ${ip-br_rtr}
+web     IN   A    ${ip_br_rtr}
 
 EOF
 
