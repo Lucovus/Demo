83b92154b354b2c82d67c045c7f804e9c508263b
Lucovus <129883444+Lucovus@users.noreply.github.com>
2026-05-27 00:06:08 +0300

    Update HQ-SRV.sh

--- HQ-SRV.sh
+++ HQ-SRV.sh
@@ -11,3 +11,10 @@ useradd -u $id_sshuser sshuser
 echo "sshuser:P@ssw0rd" | chpasswd
 usermod -aG wheel sshuser
 echo "WHEEL_USERS ALL=(ALL:ALL) NOPASSWD: ALL" > /etc/sudoers.d/sshuser
+
+echo "Authorized access only" > /etc/openssh/banner
+read -p "какой порт у ssh по заданию? Без пробелов " port_ssh
+read -p "Сколько попыток авторизации по заданию? Без пробелов только цифру " Max_auth
+echo -e "Port ${port_ssh}\nMaxAuthTries ${Max_auth}\nAllowUsers sshuser\nBanner /etc/openssh/banner\n" >> /etc/openssh/sshd_config
+systemctl restart sshd
+exec bash
