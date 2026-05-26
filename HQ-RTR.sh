af0f87eabb6c1960344e30943df82f1d91a12f0b
Lucovus <129883444+Lucovus@users.noreply.github.com>
2026-05-26 06:14:39 +0300

    HQ-RTR.sh

--- HQ-RTR.sh
+++ HQ-RTR.sh
@@ -55,6 +55,9 @@ echo "$ip_vlan100" > /etc/net/ifaces/vlan100/ipv4address
 echo "$ip_vlan200" > /etc/net/ifaces/vlan200/ipv4address
 echo "$ip_vlan100" > /etc/net/ifaces/vlan999/ipv4address
 
+iptables -t nat -A PREROUTING -i $int_type -p tcp --dport 2026 -j DNAT --to-destination $ip_vlan100
+iptables -t nat -A PREROUTING -i $int_type -p tcp --dport 8080 -j DNAT --to-destination ${ip_vlan100}:80
+
 useradd net_admin
 echo "net_admin:P@ssw0rd" | chpasswd
 touch /etc/sudoers.d/net_admin
