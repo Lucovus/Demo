246fc48b9520dc3b241a3ee95995a227c4841660
Lucovus <129883444+Lucovus@users.noreply.github.com>
2026-05-26 23:59:11 +0300

    Update BR-SRV.sh

--- BR-SRV.sh
+++ BR-SRV.sh
@@ -3,6 +3,7 @@ say() {
         echo "$1" | iconv -f utf-8 -t cp1251 2>/dev/null || echo "$1"
 }
 hostnamectl hostname br-srv.au-team.irpo
+apt-get update && apt-get install sudo 
 read -p "какой индентификатор у пользователя sshuser по заданию? Без пробелов " id_sshuser
 useradd -u $id_sshuser sshuser
 echo "sshuser:P@ssw0rd" | chpasswd
