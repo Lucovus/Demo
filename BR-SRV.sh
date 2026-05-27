df55e403f3059e4e4b6b83fa65f5c2056d11f611
Lucovus <129883444+Lucovus@users.noreply.github.com>
2026-05-28 01:41:16 +0300

    Update BR-SRV.sh

--- BR-SRV.sh
+++ BR-SRV.sh
@@ -14,5 +14,28 @@ echo "Authorized access only" > /etc/openssh/banner
 read -p "какой порт у ssh по заданию? Без пробелов " port_ssh
 read -p "Сколько попыток авторизации по заданию? Без пробелов только цифру " Max_auth
 echo -e "Port ${port_ssh}\nMaxAuthTries ${Max_auth}\nAllowUsers sshuser\nBanner /etc/openssh/banner\n" >> /etc/openssh/sshd_config
+
+echo -e "\e[32Установка SambaAD \e["
+apt-get update && apt-get install -y task-samba-dc
+rm -f /etc/samba/smb.conf
+rm -rf {/var/lib/samba, /var/cache/samba}
+mkdir -p /var/lib/samba/sysvol
+samba-tool domain provision
+
+mv /etc/krb5.conf /etc/krb5.conf.back 
+cp /var/lib/samba/private/krb5.conf /etc/krb5.conf 
+
+sed -i 's/nameserver 8.8.8.8/nameserver 127.0.0.1/' /etc/net/ifaces/enp7s1/resolv.conf; systemctl restart network; cat /etc/resolv.conf
+
+echo -e "\e[33Внимание! в DNS backend укажи IP HQ-SRV !!!\e["
+
+samba-tool dns add br-srv.au-team.irpo au-team.irpo hq-srv A 192.168.1.10 -U Administrator
+samba-tool dns add br-srv.au-team.irpo au-team.irpo hq-rtr A 192.168.1.1 -U Administrator
+samba-tool dns add br-srv.au-team.irpo au-team.irpo br-rtr A 192.168.3.1 -U Administrator
+samba-tool dns add br-srv.au-team.irpo au-team.irpo web.au-team.irpo A 172.16.1.1 -U Administrator
+samba-tool dns add br-srv.au-team.irpo au-team.irpo docker.au-team.irpo A 172.16.2.1 -U Administrator
+
+
+systemctl enable --now samba 
 systemctl restart sshd
 exec bash
