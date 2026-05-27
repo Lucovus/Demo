e320ad5ae0d13899697495726d593351b4587259
Lucovus <129883444+Lucovus@users.noreply.github.com>
2026-05-28 01:32:48 +0300

    Update HQ-CLI.sh

--- HQ-CLI.sh
+++ HQ-CLI.sh
@@ -3,3 +3,5 @@ say() {
         echo "$1" | iconv -f utf-8 -t cp1251 2>/dev/null || echo "$1"
 }
 hostnamectl hostname HQ-CLI.au-team.irpo
+
+apt-get update && apt-get install yandex-browser-stable -y 
