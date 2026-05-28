432f54b4c35b8a19a30acd40a35a056582296692
Lucovus <129883444+Lucovus@users.noreply.github.com>
2026-05-28 11:08:41 +0200

    Update HQ-CLI.sh

--- HQ-CLI.sh
+++ HQ-CLI.sh
@@ -2,6 +2,14 @@
 say() {
         echo "$1" | iconv -f utf-8 -t cp1251 2>/dev/null || echo "$1"
 }
-hostnamectl hostname HQ-CLI.au-team.irpo
+hostnamectl hostname hq-cli.au-team.irpo
 
 apt-get update && apt-get install yandex-browser-stable -y 
+
+mkdir /mnt/nfs
+chmod -R 777 /mnt/nfs
+showmount -e hq-srv
+cp /etc/fstab /etc/fstab.back
+echo "192.168.1.10:/raid/nfs /mnt/nfs nfs rw,soft,_netdev 0 0	" >> /etc/fstab
+mount -av
+df -T
