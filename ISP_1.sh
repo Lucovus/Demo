b3ff57f419175b46103bc91fa2b68649829652c2
Lucovus <129883444+Lucovus@users.noreply.github.com>
2026-06-05 20:49:22 +0300

    Update ISP_1.sh

--- ISP_1.sh
+++ ISP_1.sh
@@ -9,7 +9,7 @@ case $Hypervisor in
     int_type="ens33"
     ;;
   2)
-    int_type="enp2s1"
+    int_type="enp7s1"
     ;;
   *)
     echo "Неправльно. напиши 1 или 2"
@@ -38,51 +38,13 @@ else
   echo ""
 fi
 if [[ $Hypervisor == 2 ]]; then
-  read -p "какой ip в сторону HQ-RTR? пиши без пробелов. пример: 172.25.1.1/30 " ip_enp2s2
-  read -p "какой ip в сторону BQ-RTR? пиши без пробелов. пример: 172.26.1.1/30 " ip_enp2s3
   mkdir /etc/net/ifaces/enp2s2
   mkdir /etc/net/ifaces/enp2s3
-  echo -e "BOOTPROTO=static\nTYPE=eth" > /etc/net/ifaces/enp2s2/options
-  echo -e "BOOTPROTO=static\nTYPE=eth" > /etc/net/ifaces/enp2s3/options
-  echo "$ip_enp2s2" > /etc/net/ifaces/enp2s2/ipv4address
-  echo "$ip_enp2s3" > /etc/net/ifaces/enp2s3/ipv4address
+  echo -e "BOOTPROTO=static\nTYPE=eth" > /etc/net/ifaces/enp7s2/options
+  echo -e "BOOTPROTO=static\nTYPE=eth" > /etc/net/ifaces/enp7s3/options
+  echo "172.16.70.1" > /etc/net/ifaces/enp2s2/ipv4address
+  echo "172.16.80.1" > /etc/net/ifaces/enp2s3/ipv4address
 fi
-apt-get update && apt-get install nginx -y
-cat << EOF > /etc/nginx/sites-available.d/r-proxy.conf
-server {
-    listen 80;
-    server_name web.au-team.irpo;
 
-    location / {
-        proxy_pass http://${ip_enp2s2%/*}:8080;
-        proxy_set_header Host \$host;
-        proxy_set_header X-Real-IP \$remote_addr;
-        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-        proxy_set_header X-Forwarded-Proto \$scheme;
-
-        auth_basic "Restricted Access";
-        auth_basic_user_file /etc/nginx/.htpasswd;
-    }
-}
-
-server {
-    listen 80;
-    server_name docker.au-team.irpo;
-
-    location / {
-        proxy_pass http://${ip_enp2s3%/*}:8080;
-        proxy_set_header Host \$host;
-        proxy_set_header X-Real-IP \$remote_addr;
-        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-        proxy_set_header X-Forwarded-Proto \$scheme;
-    }
-}
-EOF
-echo -e "\e[32mNginx файл конфига готов\e[0m"
-ln -s /etc/nginx/sites-available.d/r-proxy.conf /etc/nginx/sites-enabled.d/
-systemctl enable --now nginx
-apt-get update && apt-get install apache2-htpasswd -y
-read -p "Какой пользователь указан в задани для Apache2? Напиши без пробелов. Далее тебе предложит пароль" user_apache
-htpasswd -c /etc/nginx/.htpasswd $user_apache
 systemctl restart network
 exec bash
