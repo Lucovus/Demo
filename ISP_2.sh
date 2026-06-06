4ed582b0a71bb154ad0e9abdb67571f75754f79a
Lucovus <129883444+Lucovus@users.noreply.github.com>
2026-06-06 05:41:34 +0300

    Update ISP_2.sh

--- ISP_2.sh
+++ ISP_2.sh
@@ -1,3 +1,25 @@
+#!/bin/bash
+say() {
+        echo "$1" | iconv -f utf-8 -t cp1251 2>/dev/null || echo "$1"
+}
+echo -e "1) VMware\n2) Proxmox"
+read -p "Выберите вариант: (1 или 2)" Hypervisor
+case $Hypervisor in
+  1)
+    int_type="ens33"
+    ;;
+  2)
+    int_type="enp7s1"
+    ;;
+  *)
+    echo "Неправльно. напиши 1 или 2"
+    exit 1 
+    ;;
+esac
+echo ""
+echo -e "\e[32mИспользуется $int_type\e[0m"
+echo ""
+
 if ip link show $int_type up &>/dev/null; then
   iptables -t nat -A POSTROUTING -o $int_type -j MASQUERADE
   /etc/init.d/iptables save
@@ -8,14 +30,12 @@ else
   echo ""
 fi
 if [[ $Hypervisor == 2 ]]; then
-  read -p "какой ip в сторону HQ-RTR? пиши без пробелов. пример: 172.25.1.1/30 " ip_enp2s2
-  read -p "какой ip в сторону BQ-RTR? пиши без пробелов. пример: 172.26.1.1/30 " ip_enp2s3
-  mkdir /etc/net/ifaces/enp2s2
-  mkdir /etc/net/ifaces/enp2s3
-  echo -e "BOOTPROTO=static\nTYPE=eth" > /etc/net/ifaces/enp2s2/options
-  echo -e "BOOTPROTO=static\nTYPE=eth" > /etc/net/ifaces/enp2s3/options
-  echo "$ip_enp2s2" > /etc/net/ifaces/enp2s2/ipv4address
-  echo "$ip_enp2s3" > /etc/net/ifaces/enp2s3/ipv4address
+  mkdir /etc/net/ifaces/enp7s2
+  mkdir /etc/net/ifaces/enp7s3
+  echo -e "BOOTPROTO=static\nTYPE=eth" > /etc/net/ifaces/enp7s2/options
+  echo -e "BOOTPROTO=static\nTYPE=eth" > /etc/net/ifaces/enp7s3/options
+  echo "172.16.70.1" > /etc/net/ifaces/enp7s2/ipv4address
+  echo "172.16.80.1" > /etc/net/ifaces/enp7s3/ipv4address
 fi
 apt-get update && apt-get install nginx -y
 cat << EOF > /etc/nginx/sites-available.d/r-proxy.conf
@@ -24,7 +44,7 @@ server {
     server_name web.au-team.irpo;
 
     location / {
-        proxy_pass http://${ip_enp2s2%/*}:8080;
+        proxy_pass http://172.16.70.1:8084;
         proxy_set_header Host \$host;
         proxy_set_header X-Real-IP \$remote_addr;
         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
@@ -40,7 +60,7 @@ server {
     server_name docker.au-team.irpo;
 
     location / {
-        proxy_pass http://${ip_enp2s3%/*}:8080;
+        proxy_pass http://172.16.80.1:8084;
         proxy_set_header Host \$host;
         proxy_set_header X-Real-IP \$remote_addr;
         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
@@ -52,7 +72,7 @@ echo -e "\e[32mNginx файл конфига готов\e[0m"
 ln -s /etc/nginx/sites-available.d/r-proxy.conf /etc/nginx/sites-enabled.d/
 systemctl enable --now nginx
 apt-get update && apt-get install apache2-htpasswd -y
-read -p "Какой пользователь указан в задани для Apache2? Напиши без пробелов. Далее тебе предложит пароль" user_apache
-htpasswd -c /etc/nginx/.htpasswd $user_apache
+htpasswd -c /etc/nginx/.htpasswd web4c
+echo "ПарОль для юзера веба"
 systemctl restart network
 exec bash
