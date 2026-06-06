6850c7e70cb7200756556a15b436d3998b6dbb0c
Lucovus <129883444+Lucovus@users.noreply.github.com>
2026-06-06 06:18:43 +0300

    Update HQ-RTR_2.sh

--- HQ-RTR_2.sh
+++ HQ-RTR_2.sh
@@ -4,3 +4,6 @@ systemctl disable --now nftables
 iptables -t nat -A POSTROUTING -o enp7s1 -j MASQUERADE
 iptables -t nat -A PREROUTING -i enp7s1 -p tcp --dport 2014 -j DNAT --to-destination 192.168.100.2
 iptables -t nat -A PREROUTING -i enp7s1 -p tcp --dport 8084 -j DNAT --to-destination 192.168.100.2:80
+
+/etc/init.d/iptables save
+systemctl enable --now iptables
