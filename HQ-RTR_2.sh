4e185fcb0a512697cb39b7ce496bde11a975d5fc
Lucovus <129883444+Lucovus@users.noreply.github.com>
2026-06-06 06:17:58 +0300

    Create HQ-RTR_2.sh

---
+++ HQ-RTR_2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+apt-get update && apt-get install iptables -y
+systemctl disable --now nftables
+iptables -t nat -A POSTROUTING -o enp7s1 -j MASQUERADE
+iptables -t nat -A PREROUTING -i enp7s1 -p tcp --dport 2014 -j DNAT --to-destination 192.168.100.2
+iptables -t nat -A PREROUTING -i enp7s1 -p tcp --dport 8084 -j DNAT --to-destination 192.168.100.2:80
