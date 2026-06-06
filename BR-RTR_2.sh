1324e474f6deccab25e459bf91cf8c6393b4fb0a
Lucovus <129883444+Lucovus@users.noreply.github.com>
2026-06-06 06:13:58 +0300

    Update BR-RTR_2.sh

--- BR-RTR_2.sh
+++ BR-RTR_2.sh
@@ -1,2 +1,13 @@
+#!/bin/bash
 
-iptables -t nat -A PREROUTING -i $int_type -p tcp -m multiport --dports 8084,2014 -j DNAT --to-destination 192.168.1.1
+systemctl disable --now nftables
+apt-get update && apt-get install iptables -y
+iptables -t nat -A PREROUTING -i enp7s1 -p tcp -m multiport --dports 8084,2014 -j DNAT --to-destination 192.168.1.1
+echo ""
+echo -e "\e[32mОбновлён репозиторий. установлен iptables\e[0m"
+echo ""
+systemctl restart network
+
+iptables -t nat -A POSTROUTING -o enp7s1 -j MASQUERADE
+/etc/init.d/iptables save
+systemctl enable --now iptables
